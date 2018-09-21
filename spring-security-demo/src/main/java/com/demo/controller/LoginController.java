b45ecd6a4129e948d50f64f137958a75ceb094c1
Rai <brai-a@ceb.com>
2018-09-21 13:43:28 +0000

    Added logout in dashboard

--- spring-security-demo/src/main/java/com/demo/controller/LoginController.java
+++ spring-security-demo/src/main/java/com/demo/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,5 +13,12 @@ public class LoginController {
 	@GetMapping("/login")
 	public String login() {
 		return "login";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		
+		session.invalidate();
+		return "logout";
 	}
 }
